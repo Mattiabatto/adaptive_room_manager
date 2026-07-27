2d27d99f15cf906313ccf600ea36c2209aa798c0
Mattiabatto <mattia.battoraro@gmail.com>
2026-07-27 15:26:02 +0200

    Delete custom_components/adaptive_room_manager directory

--- custom_components/adaptive_room_manager/entity.py
+++
@@ -1,9 +0,0 @@
-"""Base entities."""
-from homeassistant.helpers.entity import Entity
-from .const import DOMAIN, CONF_AREA_ID
-class ArmEntity(Entity):
-    _attr_has_entity_name=True
-    def __init__(self,manager,key):
-        self.manager=manager; self._attr_unique_id=f"{manager.entry.entry_id}_{key}"
-    async def async_added_to_hass(self): self.manager.add_callback(self.async_write_ha_state)
-    async def async_will_remove_from_hass(self): self.manager.remove_callback(self.async_write_ha_state)
